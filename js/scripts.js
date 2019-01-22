2b515521da6fab3a3a000fc0b7cf8a846ee588f7
Gary Bermudez and Ryan McLean <gary@>
2019-01-22 14:30:32 -0800

    add looping method

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
   $("#favoriteThing").submit(function(event) {
-    var favoriteFood = $("input#favoriteFood").val();
-    console.log(favoriteFood);
-    var favoriteColor = $("input#favoriteColor").val();
-    // console.log(favoriteColor);
-    var favoriteMovie = $("input#favoriteMovie").val();
-    console.log(favoriteMovie);
-    var stuff = [['1',favoriteFood],['1',favoriteColor],['1',favoriteMovie],];
+    var stuff = ["favoriteFood","favoriteColor",'favoriteMovie'];
+    stuff.forEach(function(favorite) {
+      var userInput = $("input#" + favorite).val();
+      $("ul").append("<li>" + userInput + "</li>");
+    });
 
-    var results = document.getElementById("list");
+      $("#results").show();
 
-    for(var i=0;i<stuff.length;i++){
-      var item = stuff[i];
-      var elem = document.createElement("li");
-        elem.value=item[0];
-        elem.innerHTML= item[1];
-
-        results.appendChild(elem);
-    }
-
-    $("#results").show();
-
-  event.preventDefault();
+    event.preventDefault();
+    });
   });
-});
+
+
+//
+    // var favoriteFood = $("input#favoriteFood").val();
+    // console.log(favoriteFood);
+    // var favoriteColor = $("input#favoriteColor").val();
+    // // console.log(favoriteColor);
+    // var favoriteMovie = $("input#favoriteMovie").val();
+    // console.log(favoriteMovie);
+    // var results = document.getElementById("list");
+    //
+    // for(var i=0;i<stuff.length;i++){
+    //   var item = stuff[i];
+    //   var elem = document.createElement("li");
+    //     elem.value=item[0];
+    //     elem.innerHTML= item[1];
+    //
+    //     results.appendChild(elem);
